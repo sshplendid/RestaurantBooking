b934c5792de141efcedb40bea61a85f98e09e615
nubang <nubang7@gmail.com>
2018-09-05 02:23:42 +0900

    source add

---
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -0,0 +1,15 @@
+package com.sds.cleancode.restaurant;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class BookingSchedulerTest {
+
+	@Test
+	public void testName() throws Exception {
+		fail();
+	}
+}
