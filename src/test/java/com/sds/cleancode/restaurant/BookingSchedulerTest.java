e544f19c67ce69db4edd602fd10dde6eeb2e37cd
'youmebae' <youme.bae@samsung.com>
2018-09-19 19:59:51 +0900

    Over Capacity

--- src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
+++ src/test/java/com/sds/cleancode/restaurant/BookingSchedulerTest.java
@@ -4,22 +4,26 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class BookingSchedulerTest {
+	private static final int NUMBER_OF_PEOPLE = 1;
+	private static final int CAPACITY = 3;
+	public static final Customer CUSTOMER = new Customer("NAME", "010-1111-1111");
+	public static final DateTime NOT_ON_THE_HOUR = new DateTime(2018,9,19,15,5);
+	public static final DateTime ON_THE_HOUR = new DateTime(2018,9,19,15,0);
 
+	BookingScheduler bookingScheduler = new BookingScheduler(CAPACITY);
+	
 	@Test(expected=RuntimeException.class)
 	public void throwAnExceptionWhenBookingTimeIsNotOnTheHour() throws Exception {
 		// arrange
-		BookingScheduler bookingScheduler
-			= new BookingScheduler(3);
-		DateTime dateTime 
-			= new DateTime(2018,9,19,15,5);
-		Customer customer 
-			= new Customer("NAME", "010-1111-1111");
 		Schedule schedule = 
-				new Schedule(dateTime, 1, customer);
+				new Schedule(NOT_ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
 		
 		// act
 		bookingScheduler.addSchedule(schedule );
@@ -30,14 +34,8 @@ public class BookingSchedulerTest {
 	@Test
 	public void scheduleCanBeAddedWhenBookingTimeIsOnTheHour() throws Exception {
 		// arrange
-		BookingScheduler bookingScheduler
-			= new BookingScheduler(3);
-		DateTime dateTime 
-			= new DateTime(2018,9,19,15,0);
-		Customer customer 
-			= new Customer("NAME", "010-1111-1111");
 		Schedule schedule = 
-				new Schedule(dateTime, 1, customer);
+				new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
 		
 		// act
 		bookingScheduler.addSchedule(schedule);
@@ -46,4 +44,29 @@ public class BookingSchedulerTest {
 		assertThat(bookingScheduler.hasSchedule(schedule),
 							is(true));
 	}
+	
+	
+	@Test(expected=RuntimeException.class)
+	public void throwAnExceptionWhenCapacityPerHourIsOver() throws Exception {
+		// arrange
+		
+		List<Schedule> schedules 
+			= new ArrayList<Schedule>();
+		Schedule fullSchedule 
+			= new Schedule(ON_THE_HOUR, CAPACITY, CUSTOMER);
+		schedules.add(fullSchedule );
+		bookingScheduler.setSchedules(schedules );
+		
+		Schedule schedule = 
+				new Schedule(ON_THE_HOUR, NUMBER_OF_PEOPLE, CUSTOMER);
+		
+		// act
+		bookingScheduler.addSchedule(schedule);
+		
+		// assert
+//		assertThat(bookingScheduler.hasSchedule(schedule),
+//							is(true));
+	}
+	
+	
 }
